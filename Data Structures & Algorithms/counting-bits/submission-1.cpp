2f7140d839deb4a59ff59f4f792c2c8248e07525
Rahul Bhatnagar <rahulbhatnagar364@gmail.com>
2026-06-27 13:12:18 +0530

    Add: counting-bits - submission-1

---
+++ Data Structures & Algorithms/counting-bits/submission-1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int>ans;
+        int cnt=0;
+        for(int i=0;i<=n;i++){
+            cnt=__builtin_popcount(i);
+            ans.push_back(cnt);
+            
+        }
+        return ans;
+    }
+};
